93c59021998356654d61a4076c1c98379f7d41d6
Gaby Campos <gabycamposcontact@gmail.com>
2023-05-16 16:36:52 -0700

    added 404 page and simplified syntax

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,9 @@
-const Home = () => {
-    return (
+const Home = () =>(
         <>
             <h1>Gumball Travels</h1>
             <p>Previous Adventure</p>
             <p>Next Adventure</p>
         </>
-    )
-}
+    );
 
 export default Home;

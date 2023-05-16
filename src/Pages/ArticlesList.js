41ba9507750c8de0dd31651f6671b90dcaca15ab
Gaby Campos <gabycamposcontact@gmail.com>
2023-05-16 16:04:20 -0700

    added new articles component

--- src/Pages/ArticlesList.js
+++ src/Pages/ArticlesList.js
@@ -1,17 +1,11 @@
+import ArticlesFromList from "../Components/ArticlesFromList";
 import articles from "./article-content";
-import { Link } from "react-router-dom";
 
 const ArticlesList = () => {
   return (
     <>
       <h1>Articles</h1>
-      {articles.map(article => (
-        //to make sure the link is to the article from the specific param
-        <Link key={article.name} className="article-list-item" to={`/articles/${article.name}`}>
-          <h3>{article.title}</h3>
-          <p>{article.content[0].substring(0,150)}...</p>
-        </Link>
-      ))}
+      <ArticlesFromList articles={articles}/>
     </>
   );
 };
